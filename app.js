6336093c31345b8a02ed585d1d7f8692ef20f235
vimal-vs <60288021+vimal-vs@users.noreply.github.com>
2022-10-06 22:31:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyASMnZJCZPTWPTOOJntw_KD8N6nP0gDMUY",
-  authDomain: "vimalvs.firebaseapp.com",
-  projectId: "vimalvs",
-  storageBucket: "vimalvs.appspot.com",
-  messagingSenderId: "268884465108",
-  appId: "1:268884465108:web:b97def5f5c1f9831eab241",
-  measurementId: "G-S3SBFB27PS"
+  //firebase config
 };
 
 // Initialize Firebase
@@ -28,9 +22,9 @@ $('#exampleModal').on('show.bs.modal', function (event) {
 function sendMail(){
   Email.send({
       Host : "smtp.elasticemail.com",
-      Username : "passgen.pybot@gmail.com",
-      Password : "5F2A4A21A5273FE9A98F4626313BECF4E21D",
-      To : "vv4861@srmist.edu.in",
+      Username : "",
+      Password : "",
+      To : "",
       From : document.getElementById('email').value,
       Subject : "Portfolio : Contact Form",
       Body : document.getElementById('message-text').value,
